51654ab7ddb07f355745c5522dd8322315b74451
mgitamdmin <134402441+mgitamdmin@users.noreply.github.com>
2024-08-01 14:55:03 -0400

    Add files via upload

---
+++ PCI policy new/main.tf
@@ -0,0 +1,20 @@
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+features {}
+#    use ENV VARS
+    subscription_id = "${var.subscription_id}"
+    client_id       = "${var.client_id}"
+    client_secret   = "${var.client_secret}"
+    tenant_id       = "${var.tenant_id}"
+	# whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+    version = "=2.5.0"
+}
+
+data "azurerm_subscription" "current" {}
+
+resource "azurerm_policy_assignment" "example" {
+  name                 = "PCI v3.2.1:2018"
+  scope                = data.azurerm_subscription.current.id
+  policy_definition_id = "/providers/Microsoft.Authorization/policySetDefinitions/496eeda9-8f2f-4d5e-8dfd-204f0a92ed41"
+}
+
